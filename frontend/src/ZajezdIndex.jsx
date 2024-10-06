a6600f6f97da2312a7d286a72ab031d5b9a072de
spejlickar <sapik@seznam.cz>
2024-10-06 16:16:23 +0200

    doplneni souboru na commit

---
+++ frontend/src/ZajezdIndex.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const ZajezdIndex = () => {
+ 
+
+  return (
+    <div>
+      
+      <h1>Ahoj svete</h1>
+    </div>
+  );
+};
+
+export default ZajezdIndex;
